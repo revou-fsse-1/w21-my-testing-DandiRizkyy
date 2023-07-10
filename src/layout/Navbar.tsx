8a586a1a27d499661a74120dec00955ef6920d40
DandiRizkyy <dandirizkyy94@gmail.com>
2023-07-10 13:11:09 +0700

    fixing navbar test

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import LogoutButton from "@/components/LogoutButton";
+import LogoutButton from "../components/LogoutButton";
 import Link from "next/link";
 import React from "react";
 
--- src/layout/__test__/Navbar.test.tsx
+++ src/layout/__test__/Navbar.test.tsx
@@ -1,16 +1,27 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"; // Import the extend-expect module
 import Navbar from "../Navbar";
-import LogoutButton from "@/components/LogoutButton";
+import LogoutButton from "../../components/LogoutButton";
 import React from "react";
-
+import { vi } from "vitest";
+const pushMock = vi.fn();
 test("renders navbar correctly", () => {
-  const { getByText } = render(
+  vi.mock("next/router", () => ({
+    useRouter: () => ({
+      push: pushMock,
+    }),
+  }));
+  const { getByText, getAllByText } = render(
     <Navbar>
       <LogoutButton />
     </Navbar>
   );
 
-  expect(getByText("Toko-ku")).toBeDefined();
+  //   expect(getAllByText("Toko-ku")).toBeDefined();
   expect(getByText("Home")).toBeDefined();
+  expect(getByText("Add Product")).toBeDefined();
+  expect(getByText("Edit Product")).toBeDefined();
+  expect(getByText("Login User")).toBeDefined();
+  expect(getByText("Login Admin")).toBeDefined();
+  expect(getByText("Logout")).toBeDefined();
 });
