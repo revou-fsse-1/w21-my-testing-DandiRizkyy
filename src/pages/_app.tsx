99dcdc041ae1cd6011764d0d1b80bb5167144db3
Dandi Rizky <dandirizkyy94@gmail.com>
2023-07-12 15:42:32 +0700

    saving form value from login user to localstorage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/layout/Layout";
+import store from "@/redux/store";
 
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -6,10 +7,10 @@ import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </Provider>
   );
 }
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,19 +1,23 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { setCookie } from "cookies-next";
+import { useDispatch, useSelector } from "react-redux";
+import { changeEmail, changePassword, resetForm } from "@/redux/formSlice";
 
-interface FormProps {
+interface FormInput {
   email: string;
   password: string;
 }
 
 export default function Login() {
   const router = useRouter();
+  const formState = useSelector<{ form: FormInput }>((state) => state.form);
+  const dispatch = useDispatch();
 
   const schema = yup
     .object({
@@ -25,12 +29,13 @@ export default function Login() {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
 
-  const onSubmit: SubmitHandler<FormProps> = async (data, event: any) => {
+  const onSubmit: SubmitHandler<FormInput> = async (data, event: any) => {
     event.preventDefault();
     const response = await axios.post(
       "https://w17-our-backend-group-c-production.up.railway.app/auth/login/user",
@@ -44,9 +49,15 @@ export default function Login() {
     setCookie("token", response.data.access_token);
 
     // Redirect to the home page
+    localStorage.removeItem("form");
+    dispatch(resetForm());
     router.push("/");
     console.log(data);
   };
+
+  useEffect(() => {
+    reset(formState as FormInput);
+  }, []);
 
   return (
     <div>
@@ -65,6 +76,7 @@ export default function Login() {
                   {...register("email")} // Register email input
                   className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
                   placeholder=" "
+                  onChange={(e) => dispatch(changeEmail(e.target.value))}
                 />
                 <label
                   htmlFor="email"
@@ -87,6 +99,7 @@ export default function Login() {
                   {...register("password")} // Register password input
                   className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
                   placeholder=" "
+                  onChange={(e) => dispatch(changePassword(e.target.value))}
                 />
                 <label
                   htmlFor="password"
